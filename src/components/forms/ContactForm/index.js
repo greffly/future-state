543dc8bfd8746652435a5f70d3b156596d7ae5f9
Caitlyn <cgreffly@gmail.com>
2021-08-20 18:05:51 -0700

    add email functionality

--- src/components/forms/ContactForm/index.js
+++ src/components/forms/ContactForm/index.js
@@ -1,16 +1,7 @@
 /* eslint-disable semi */
 import React from 'react';
-import { Formik, Field } from 'formik';
-import { navigate } from 'gatsby';
-import validationSchema from './validationSchema';
 import './styles.sass';
 import '../../../styles/global.sass';
-
-const encode = (data) => {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-};
 
 const ContactForm = () => {
   return (
@@ -19,151 +10,111 @@ const ContactForm = () => {
         <h1 className='has-text-white is-size-2-desktop is-size-3-tablet is-size-4-mobile pb-3 ml-3 is-uppercase'>
           Drop us a line
         </h1>
-        <Formik
-          initialValues={{ name: '', email: '', message: '' }}
-          validationSchema={validationSchema}
-          onSubmit={(values, { setSubmitting }) => {
-            fetch('/?no-cache=1', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-              body: encode({
-                'form-name': 'contact',
-                ...values,
-              }),
-            })
-              .then(() => {
-                navigate('/contact/success');
-                setSubmitting(false);
-              })
-              .catch((error) => {
-                console.log(error);
-                alert('Error: Please Try Again!'); //eslint-disable-line
-                setSubmitting(false);
-              });
-          }}
-          render={({
-            errors,
-            touched,
-            isSubmitting,
-            handleSubmit,
-            handleReset,
-          }) => (
-            <form
-              name='contact'
-              id='contact-form'
-              onSubmit={handleSubmit}
-              onReset={handleReset}
-              data-netlify='true'
-              data-netlify-honeypot='bot-field'
-            >
-              <div className='columns'>
-                <div className='column m-3'>
-                  <div className='field'>
-                    <label className='label has-text-info is-size-5'>
-                      Name
-                    </label>
-                    <div className='control'>
-                      <Field
-                        className='input is-info has-background-grey-dark'
-                        type='text'
-                        placeholder='Type Something'
-                        name='name'
-                        id='name'
-                      />
-                    </div>
-                    {touched.name && errors.name && (
-                      <small className='has-text-danger'>{errors.name}</small>
-                    )}
-                  </div>
-
-                  <div className='field pt-4'>
-                    <label className='label has-text-info is-size-5'>
-                      Email
-                    </label>
-                    <div className='control'>
-                      <Field
-                        className='input is-info has-background-grey-dark'
-                        type='email'
-                        placeholder='Type Something'
-                        name='email'
-                        id='email'
-                      />
-                    </div>
-                    {touched.email && errors.email && (
-                      <small className='has-text-danger'>{errors.email}</small>
-                    )}
-                  </div>
-
-                  <div className='field pt-4'>
-                    <label className='label has-text-info is-size-5'>
-                      Company (Optional)
-                    </label>
-                    <div className='control'>
-                      <Field
-                        className='input is-info has-background-grey-dark'
-                        type='text'
-                        placeholder='Type Something'
-                        name='message'
-                        id='message'
-                        rows='6'
-                      />
-                    </div>
-                    {touched.message && errors.message && (
-                      <small className='has-text-danger'>
-                        {errors.message}
-                      </small>
-                    )}
-                  </div>
-                </div>
-
-                <div className='column m-3'>
-                  <label className='label has-text-info is-size-5'>
-                    About You
-                  </label>
-                  <div className='control pb-2 custom-talent-radio'>
-                    <label className='radio has-text-info pr-5'>
-                      <input type='radio' name='answer' className='mr-2' />
-                      Looking for talent
-                    </label>
-                    <div>
-                      <label className='radio has-text-info'>
-                        <input type='radio' name='answer' className='mr-2' />I
-                        am the talent
-                      </label>
-                    </div>
-                  </div>
-
-                  <div className='field pt-4'>
-                    <label className='label has-text-info is-size-5'>
-                      Comment
-                    </label>
-                    <div className='control'>
-                      <Field
-                        className='textarea is-info has-background-grey-dark custom-textarea'
-                        component='textarea'
-                        placeholder='Type Something'
-                        name='message'
-                        id='message'
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-
+        <form
+          method='POST'
+          action='https://getform.io/f/66772fd8-35a0-4090-8de4-a1808af5f46d'
+          name='contact'
+          id='contact-form'
+          data-netlify='true'
+          data-netlify-honeypot='bot-field'
+        >
+          <div className='columns'>
+            <div className='column m-3'>
               <div className='field'>
-                <div className='control column is-2 p-0'>
-                  <button
-                    className='button is-info p-5 is-size-5 is-fullwidth mt-3 form-submit-button'
-                    type='submit'
-                    disabled={isSubmitting}
-                  >
-                    SEND
-                  </button>
+                <label className='label has-text-info is-size-5'>Name</label>
+                <div className='control'>
+                  <input
+                    className='input has-text-info is-info has-background-grey-dark'
+                    type='text'
+                    placeholder='Type Something'
+                    name='Name'
+                    id='Name'
+                  />
                 </div>
               </div>
-            </form>
-          )}
-        />
+
+              <div className='field pt-4'>
+                <label className='label has-text-info is-size-5'>Email</label>
+                <div className='control'>
+                  <input
+                    className='input has-text-info is-info has-background-grey-dark'
+                    type='email'
+                    placeholder='Type Something'
+                    name='Email'
+                    id='Email'
+                  />
+                </div>
+              </div>
+
+              <div className='field pt-4'>
+                <label className='label has-text-info is-size-5'>
+                  Company (Optional)
+                </label>
+                <div className='control'>
+                  <input
+                    className='input has-text-info is-info has-background-grey-dark'
+                    type='text'
+                    placeholder='Type Something'
+                    name='Company'
+                    id='Company'
+                    rows='6'
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className='column m-3'>
+              <label className='label has-text-info is-size-5'>About You</label>
+              <div className='control pb-2 custom-talent-radio'>
+                <label className='radio has-text-info pr-5'>
+                  <input
+                    type='radio'
+                    name='Talent'
+                    value='Looking for talent'
+                    className='mr-2'
+                  />
+                  Looking for talent
+                </label>
+                <div>
+                  <label className='radio has-text-info'>
+                    <input
+                      type='radio'
+                      name='Talent'
+                      value='I am the talent'
+                      className='mr-2'
+                    />
+                    I am the talent
+                  </label>
+                </div>
+              </div>
+
+              <div className='field pt-4'>
+                <label className='label has-text-info is-size-5'>Comment</label>
+                <div className='control'>
+                  <textarea
+                    className='textarea has-text-info is-info has-background-grey-dark custom-textarea'
+                    component='textarea'
+                    placeholder='Type Something'
+                    name='Comment'
+                    id='Comment'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='field'>
+            <div className='control column is-2 p-0'>
+              <button
+                className='button is-info p-5 is-size-5 is-fullwidth mt-3 form-submit-button'
+                type='submit'
+              >
+                SEND
+              </button>
+            </div>
+          </div>
+          {/* {successMessage && <Alert color='success'>Message Sent!</Alert>} */}
+        </form>
       </div>
     </section>
   );
