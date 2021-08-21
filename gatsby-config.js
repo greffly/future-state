5b96d8f736fba8f47233c20c3c7c06bf7298533c
Caitlyn <cgreffly@gmail.com>
2021-08-20 17:30:45 -0700

    testing font

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,7 +52,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         typekit: {
-          id: 'cws6hwr',
+          id: process.env.TYPEKIT_ID,
         },
       },
     },
